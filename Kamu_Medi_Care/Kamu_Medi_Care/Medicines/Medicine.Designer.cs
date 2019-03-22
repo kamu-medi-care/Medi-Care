34bed09b9b9ed730eecd1346330205796a66ffa4
khubaib latif <Khubaib.5134407@talmeez.pk>
2019-03-22 14:56:04 +0500

    medicine form design

--- Kamu_Medi_Care/Kamu_Medi_Care/Medicines/Medicine.Designer.cs
+++ Kamu_Medi_Care/Kamu_Medi_Care/Medicines/Medicine.Designer.cs
@@ -29,61 +29,26 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lblMedicineName = new System.Windows.Forms.Label();
-            this.txtMedicineName = new System.Windows.Forms.TextBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.MedicineColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnAdd = new System.Windows.Forms.Button();
+            this.dgvMedicine = new System.Windows.Forms.DataGridView();
+            this.MedicineColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtMedicineName = new System.Windows.Forms.TextBox();
+            this.lblMedicineName = new System.Windows.Forms.Label();
             this.lblTitle = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicine)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.btnAdd);
-            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.dgvMedicine);
             this.panel1.Controls.Add(this.txtMedicineName);
             this.panel1.Controls.Add(this.lblMedicineName);
             this.panel1.Location = new System.Drawing.Point(26, 90);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(740, 340);
             this.panel1.TabIndex = 0;
-            // 
-            // lblMedicineName
-            // 
-            this.lblMedicineName.AutoSize = true;
-            this.lblMedicineName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMedicineName.Location = new System.Drawing.Point(37, 32);
-            this.lblMedicineName.Name = "lblMedicineName";
-            this.lblMedicineName.Size = new System.Drawing.Size(90, 20);
-            this.lblMedicineName.TabIndex = 0;
-            this.lblMedicineName.Text = "Medicine :";
-            // 
-            // txtMedicineName
-            // 
-            this.txtMedicineName.Location = new System.Drawing.Point(214, 32);
-            this.txtMedicineName.Multiline = true;
-            this.txtMedicineName.Name = "txtMedicineName";
-            this.txtMedicineName.Size = new System.Drawing.Size(244, 27);
-            this.txtMedicineName.TabIndex = 1;
-            // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.MedicineColumn});
-            this.dataGridView1.Location = new System.Drawing.Point(163, 110);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(383, 150);
-            this.dataGridView1.TabIndex = 2;
-            // 
-            // MedicineColumn
-            // 
-            this.MedicineColumn.HeaderText = "Medicines";
-            this.MedicineColumn.Name = "MedicineColumn";
-            this.MedicineColumn.ReadOnly = true;
-            this.MedicineColumn.Width = 340;
             // 
             // btnAdd
             // 
@@ -94,6 +59,41 @@
             this.btnAdd.TabIndex = 3;
             this.btnAdd.Text = "Add";
             this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // dgvMedicine
+            // 
+            this.dgvMedicine.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMedicine.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MedicineColumn});
+            this.dgvMedicine.Location = new System.Drawing.Point(163, 110);
+            this.dgvMedicine.Name = "dgvMedicine";
+            this.dgvMedicine.Size = new System.Drawing.Size(383, 150);
+            this.dgvMedicine.TabIndex = 2;
+            // 
+            // MedicineColumn
+            // 
+            this.MedicineColumn.HeaderText = "Medicines";
+            this.MedicineColumn.Name = "MedicineColumn";
+            this.MedicineColumn.ReadOnly = true;
+            this.MedicineColumn.Width = 340;
+            // 
+            // txtMedicineName
+            // 
+            this.txtMedicineName.Location = new System.Drawing.Point(214, 32);
+            this.txtMedicineName.Multiline = true;
+            this.txtMedicineName.Name = "txtMedicineName";
+            this.txtMedicineName.Size = new System.Drawing.Size(244, 27);
+            this.txtMedicineName.TabIndex = 1;
+            // 
+            // lblMedicineName
+            // 
+            this.lblMedicineName.AutoSize = true;
+            this.lblMedicineName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedicineName.Location = new System.Drawing.Point(37, 32);
+            this.lblMedicineName.Name = "lblMedicineName";
+            this.lblMedicineName.Size = new System.Drawing.Size(90, 20);
+            this.lblMedicineName.TabIndex = 0;
+            this.lblMedicineName.Text = "Medicine :";
             // 
             // lblTitle
             // 
@@ -119,7 +119,7 @@
             this.Text = "Medicine";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicine)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -131,7 +131,7 @@
         private System.Windows.Forms.TextBox txtMedicineName;
         private System.Windows.Forms.Label lblMedicineName;
         private System.Windows.Forms.Button btnAdd;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvMedicine;
         private System.Windows.Forms.DataGridViewTextBoxColumn MedicineColumn;
         private System.Windows.Forms.Label lblTitle;
     }
