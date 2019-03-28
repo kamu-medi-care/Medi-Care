06f0526976fd959c17d0276db218a2b597e743db
Muhammad.1562617 <Muhammad.1562617@talmeez.pk>
2019-03-28 23:18:19 +0500

    SignIN

--- Kamu_Medi_Care/Kamu_Medi_Care/LogIn/SignIn.Designer.cs
+++ Kamu_Medi_Care/Kamu_Medi_Care/LogIn/SignIn.Designer.cs
@@ -130,13 +130,13 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Magenta;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.White;
             this.label3.Location = new System.Drawing.Point(221, 57);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(100, 22);
+            this.label3.Size = new System.Drawing.Size(105, 31);
             this.label3.TabIndex = 8;
-            this.label3.Text = "User Name";
+            this.label3.Text = "Sign In";
             // 
             // SignIn
             // 
