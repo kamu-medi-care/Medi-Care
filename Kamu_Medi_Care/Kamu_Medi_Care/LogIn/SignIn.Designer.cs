0c2f727416ba93ce39f7d92f03bfa269117adc9e
Muhammad.1562617 <Muhammad.1562617@talmeez.pk>
2019-03-28 19:59:02 +0500

    Design

--- Kamu_Medi_Care/Kamu_Medi_Care/LogIn/SignIn.Designer.cs
+++ Kamu_Medi_Care/Kamu_Medi_Care/LogIn/SignIn.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SignIn));
             this.panel1 = new System.Windows.Forms.Panel();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
@@ -139,6 +140,7 @@
             this.ClientSize = new System.Drawing.Size(812, 481);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "SignIn";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- Kamu_Medi_Care/Kamu_Medi_Care/Templates/Template.Designer.cs
+++ Kamu_Medi_Care/Kamu_Medi_Care/Templates/Template.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Template));
             this.SuspendLayout();
             // 
             // Template
@@ -36,6 +37,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Magenta;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Template";
             this.Text = "Template";
             this.ResumeLayout(false);
