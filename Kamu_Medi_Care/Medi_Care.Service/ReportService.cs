6efe90304b70fac49f278814a1c2628efe0fc8f1
Muhammad.1562617 <Muhammad.1562617@talmeez.pk>
2019-04-23 13:40:08 +0500

    Report

--- Kamu_Medi_Care/Medi_Care.Service/ReportService.cs
+++ Kamu_Medi_Care/Medi_Care.Service/ReportService.cs
@@ -1,14 +1,21 @@
 ﻿using Kamu_Medi_Care.Models;
 using Medi_Care.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Medi_Care.Models
 {
     public class ReportService
     {
-        public ReceptionModel GetReport(ReportModel reportModel)
+        public List<ReceptionModel> GetReport(ReportModel reportModel)
         {
             using (var context = new MCContext())
             {
+                //var from = Convert.ToInt32(reportModel.StartDate);
+                //var to = Convert.ToInt32(reportModel.EndDate);
+                //var report = context.ReceptionModels.AsEnumerable().Skip(from)
+                //    .Take(to).ToList();
                 //var report = context.ReceptionModels.Where(c => c.DateTime== "BETWEEN" +
                 //reportModel.StartDate + "AND" +  reportModel.StartDate).ToList();
                 return null;
